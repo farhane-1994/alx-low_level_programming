55cbde980bb16e5f951d4bd6ae84ac12a2020437
farhane-1994 <lh.farhane1994@gmail.com>
2023-06-20 02:10:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
-int main() {
-    printf("_putchar\n");
-    return 0;
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
+int main(void)
+{
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }
+
+
+
+
+
