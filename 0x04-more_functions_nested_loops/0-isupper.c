eb7a8bebeac8875b816a0a30972cd47e4d3b12e0
farhane-1994 <lh.farhane1994@gmail.com>
2023-06-28 11:20:33 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,13 @@
  *
  * Return: 0 or 1
  */
-int _isupper(int c)
+int _isupper(int X)
 
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-
+	if (X >= '65' && X <= '90')
+        {	
+	return (1);
+	}
+	return (0);
 }
+
