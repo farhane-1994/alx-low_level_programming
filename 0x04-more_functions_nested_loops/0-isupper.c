acb8dd04c1c7939bb0489029c7cb2a27af4e60f9
farhane-1994 <lh.farhane1994@gmail.com>
2023-06-29 16:47:44 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _isupper - It takes one argument
  * @c: The character to be checked
@@ -9,12 +8,12 @@
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+        if (c >= 'A' && c <= 'Z')
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
 }
