551417f9854500ab90c10b1ee863321a12292432
farhane-1994 <lh.farhane1994@gmail.com>
2023-07-04 03:21:13 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Ephrem Abebe
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +19,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+
+#endif
